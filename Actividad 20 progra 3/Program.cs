f8bca7c8134f943af9dfba1cb306d3c5feceb10d
ricogm-source <ricardoggmenchu@gmail.com>
2026-04-17 08:08:47 -0600

    agregado de clase paciente

--- Actividad 20 progra 3/Program.cs
+++ Actividad 20 progra 3/Program.cs
@@ -8,6 +8,65 @@ namespace Actividad_20_progra_3
 {
     internal class Program
     {
+        class Paciente
+
+        {
+
+            private string nombre;
+
+            public string Nombre
+
+            {
+                get
+
+                {
+
+                    return nombre;
+
+                }
+
+                set
+
+                {
+
+                    if(value.Length>=3)
+
+                    {
+                        nombre = value;
+
+
+                    }
+
+                    else
+
+                    {
+
+                        Console.WriteLine("El nombre debe tener al menos tres carácteres");
+
+                    }
+
+
+
+                }
+
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
         }
