c84e7ef580b7070ef5abefd15025da01fd30e697
ricogm-source <ricardoggmenchu@gmail.com>
2026-04-17 08:04:26 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 20 progra 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_20_progra_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
